206f1fbc52e72d057adc0ed5788e3dd5d484cc2e
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-08-07 11:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,18 +31,6 @@ class Teacher extends Person {
 	}
 }
 
-const person = new Person("John,25");
-console.log(person.name);
-person.age = 30;  
-console.log(person.age);  
-
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
