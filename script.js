d48e767ba2c30b276fc2254787e1516ce186ec1d
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-08-07 12:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this._name = name;
-		this.age = age;
-	}
+  constructor(name, age) {
+    this._name = name; // use underscore to store private variable
+    this._age = age;
+  }
 
-	get name(){
-		return this.name;
-	}
-	set name(newName){
-		return this.name=newName;
-	}
-	get age(){
-		return this.age;
-	}
-	set age(newAge){
-		return this.age = newAge;
-	}
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.logs(this.name + "is studying")
-	}
+  study() {
+    console.log(this.name + " is studying");
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name + "is teaching");
-	}
+  teach() {
+    console.log(this.name + " is teaching");
+  }
 }
-
 
 // Do not change the code below this line
 window.Person = Person;
